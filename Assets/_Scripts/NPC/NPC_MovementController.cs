f555a467fdeddb64c2ccfe2abe1f25e1dd26f24d
Yuriy Balint <yurabalint0103@gmail.com>
2025-03-21 23:49:17 +0200

    NPC movement update

--- Assets/_Scripts/NPC/NPC_MovementController.cs
+++ Assets/_Scripts/NPC/NPC_MovementController.cs
@@ -4,6 +4,7 @@ using UnityEngine.AI;
 public class NPC_MovementController : MonoBehaviour
 {
     private NavMeshAgent agent;
+    private Animator animator;
     [SerializeField] private Vector3 interactionZonePosition;
     private Vector3 carPosition;
 
@@ -12,10 +13,23 @@ public class NPC_MovementController : MonoBehaviour
     {
         agent = GetComponent<NavMeshAgent>();
         interactionZonePosition = FindAnyObjectByType<NPC_InteractionZone>().transform.position;
+        animator = GetComponent<Animator>();
     }
    
 
     public void WalkToInteractionZone() => agent.SetDestination(interactionZonePosition);
     public void WalkToCar() => agent.SetDestination(carPosition);
+
+    private void Update()
+    {
+        if (agent.velocity.magnitude > 0)
+        {
+            animator.SetBool("Walking", true);
+        }
+        else
+        {
+            animator.SetBool("Walking", false);
+        }
+    }
 
 }
